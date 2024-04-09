65e1fb01d5b57045b4b2074507cfc40e90c769cb
Vriendelik <ntebohelengd@gmail.com>
2024-04-09 06:03:38 -0700

    Response header value #0

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+
 import urllib.request
 import sys
 
@@ -7,7 +8,7 @@ if __name__ == "__main__":
 
     try:
         with urllib.request.urlopen(url) as response:
-            header = response.headers.get('X-Request-Id')
-            print(header)
+            x_request_id = response.getheader('X-Request-Id')
+            print(x_request_id)
     except urllib.error.URLError as e:
         print("Error fetching URL:", e.reason)
