9257bd62461e4c9a8b6508ccb4390f76a89a658b
Vriendelik <ntebohelengd@gmail.com>
2024-04-09 05:55:33 -0700

    What's my status? #0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -5,9 +5,9 @@ url = "https://alx-intranet.hbtn.io/status"
 
 try:
     with urllib.request.urlopen(url) as response:
-        body = response.read().decode('utf-8')
-        print("Body response:")
-        print("\t- type: {}".format(type(body)))
-        print("\t- content: {}".format(body))
+    body = response.read().decode('utf-8')
+    print("Body response:")
+    print("\t- type: {}".format(type(body)))
+    print("\t- content: {}".format(body))
 except urllib.error.URLError as e:
     print("Error fetching URL:", e.reason)
